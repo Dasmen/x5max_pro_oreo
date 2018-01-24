9c548ea46dd8e3630a030d2ed73694db7f7c6dce
SeriniTY320 <SeriniTY320@gmail.com>
2018-01-24 23:02:15 +0300

    Initial commit

---
+++ out1/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 SMP PREEMPT Wed Jan 24 22:58:47 MSK 2018"
+#define LINUX_COMPILE_BY "serinity"
+#define LINUX_COMPILE_HOST "serinity-H55H-M"
+#define LINUX_COMPILER "gcc version 4.9.4 20150629 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.06 - Cortex-A7) "
